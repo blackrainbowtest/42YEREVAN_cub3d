e7ef62860a6a5ee7fa31ba6d9051f2334360b11b
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-03-04 14:17:17 +0400

    feat: add clean_exit API

---
+++ src/exit.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exot.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 16:13:47 by aramarak          #+#    #+#             */
+/*   Updated: 2026/03/04 14:16:16 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+int	clean_exit(t_data *d, int code)
+{
+	if (d->img.img && d->mlx.mlx)
+		mlx_destroy_image(d->mlx.mlx, d->img.img);
+	if (d->mlx.win && d->mlx.mlx)
+		mlx_destroy_window(d->mlx.mlx, d->mlx.win);
+	exit(code);
+	return (0);
+}
