ca02da716b56edfb1d8510093bc67ae4b9a1245a
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-03-04 14:22:41 +0400

    refactor: first check simple API usage

--- src/main.c
+++ src/main.c
@@ -12,34 +12,15 @@
 
 #include "../includes/cub3d.h"
 
-int	key_handler(int keycode, void *param)
-{
-	(void)param;
-	printf("keycode: %d\n", keycode);
-	return (0);
-}
-
-int	close_handler(void *param)
-{
-	(void)param;
-	exit(0);
-	return (0);
-}
-
 int	main(void)
 {
 	t_data	d;
 
-	d.mlx.w = 800;
-	d.mlx.h = 600;
-	d.mlx.mlx = mlx_init();
-	d.mlx.win = mlx_new_window(d.mlx.mlx, 800, 600, "cub3d");
-	d.img.img = mlx_new_image(d.mlx.mlx, d.mlx.w, d.mlx.h);
-	d.img.addr = mlx_get_data_addr(d.img.img, &d.img.bpp,
-			&d.img.line_len, &d.img.endian);
+	if (app_init(&d) != 0)
+		return (MLX_ERROR);
 	render_test(&d);
-	mlx_hook(d.mlx.win, EV_KEYDOWN, 1L << 0, key_handler, &d.mlx);
-	mlx_hook(d.mlx.win, EV_KEYUP, 0, close_handler, &d.mlx);
+	mlx_hook(d.mlx.win, EVT_KEYDOWN, 1L << 0, on_keydown, &d);
+	mlx_hook(d.mlx.win, EVT_DESTROY, 0, on_destroy, &d);
 	mlx_loop(d.mlx.mlx);
 	return (0);
 }
