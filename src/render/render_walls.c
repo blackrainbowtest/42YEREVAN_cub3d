498a42c36c5e467daaa60240aa35861d5dba85b7
blackrainbowtest <araqelyan.aram.96@gmail.com>
2026-03-14 16:19:52 +0400

    bigfix: declared line_height

--- src/render/render_walls.c
+++ src/render/render_walls.c
@@ -112,6 +112,7 @@ void	draw_column_pixels(t_data *d, int x, int start, int end, t_dda *r)
 	t_tex			*t;
 	double			wall_x;
 	int				tex_x;
+	int				wall_height;
 	double			step;
 	double			tex_pos;
 	unsigned int	color;
@@ -136,8 +137,11 @@ void	draw_column_pixels(t_data *d, int x, int start, int end, t_dda *r)
 			tex_x = t->width - tex_x - 1;
 		if (r->side == 1 && r->dir_y > 0)
 			tex_x = t->width - tex_x - 1;
-		step = (double)t->height / (double)(end - start);
-		tex_pos = (start - WINDOW_HEIGHT / 2 + line_height / 2) * step;
+		wall_height = end - start;
+		if (wall_height <= 0)
+			wall_height = 1;
+		step = (double)t->height / (double)wall_height;
+		tex_pos = (start - WINDOW_HEIGHT / 2 + wall_height / 2.0) * step;
 		while (y <= end)
 		{
 			int tex_y = (int)tex_pos;
